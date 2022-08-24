381a3db48a8d29dd9d4f220e5b02ae869cb57c1d
Lucas Henrique Dellarmelindo Ferreira <111028538+LucashenriqueDF@users.noreply.github.com>
2022-08-24 16:36:56 -0400

    Add files via upload

---
+++ projeto_calculadora/calculadora.js
@@ -0,0 +1,31 @@
+function colocar (num){
+    let numeros = document.getElementById('resultado').innerHTML 
+    document.getElementById('resultado').innerHTML = (numeros + num);
+
+}
+
+function clean( ){
+    document.getElementById('resultado').innerHTML = " ";
+
+}
+
+function voltar( ){
+let resultado =  document.getElementById('resultado').innerHTML;
+document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+
+}
+
+function calcular( ){
+   let resultado = document.getElementById('resultado').innerHTML
+
+    if(resultado){
+
+     document.getElementById('resultado').innerHTML = eval(resultado);
+
+    }
+    else{
+
+        document.getElementById('resultado').innerHTML = "Error";
+    }
+
+}
